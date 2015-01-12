f90e052cc09368ea7883aae7b6852bdcda708f74
Stefan Andres <s.andres@syseleven.de>
2015-01-12 14:39:30 +0100

    os-198 rename user.py to create_superuser.py

--- manifests/profile/server/users.pp
+++ manifests/profile/server/users.pp
@@ -1,13 +1,18 @@
-class sys11graphite::profile::users() {
-  file {'/opt/graphite/webapp/graphite/user.py':
+class sys11graphite::profile::users(
+  $superusers = {},
+) {
+  define create_superuser() {
+    notify{'TBD':}
+  }
+  file {'/opt/graphite/webapp/graphite/create_superuser.py':
     ensure => file,
     mode   => '0555',
-    source => "puppet:///modules/$module_name/user.py",
+    source => "puppet:///modules/$module_name/create_superuser.py",
     } ~>
 
   # init database mysql -e 'drop database graphite; create database graphite'; { echo  'no'; } | python /opt/graphite/webapp/graphite/manage.py syncdb
   exec {'create db and superuser':
-    command     => 'python /opt/graphite/webapp/graphite/manage.py syncdb; /opt/graphite/webapp/graphite/user.py root tf-platform@syseleven.de Ofoo9ohf6vie || exit 0',
+    command     => 'python /opt/graphite/webapp/graphite/manage.py syncdb; /opt/graphite/webapp/graphite/create_superuser.py root tf-platform@syseleven.de Ofoo9ohf6vie || exit 0',
     provider    => 'shell',
     refreshonly => true,
   }
