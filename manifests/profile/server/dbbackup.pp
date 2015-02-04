5a6a96a62c886f1d1ed6a0d66cf3341e4de8a487
Kristian Koehntopp <k.koehntopp@syseleven.de>
2015-02-04 09:38:59 +0100

    os-31 split db dump and whisper expire in two, move jobs to /usr/local/bin, remove w right from files

---
+++ manifests/profile/server/dbbackup.pp
@@ -0,0 +1,39 @@
+class sys11graphite::profile::server::dbbackup (
+  # Database dumps go here
+  $graphite_db_dumpdir     = '/root/dumps',
+  # execute dump at this cron timespec
+  $graphite_db_dump_minute   = 0,
+  $graphite_db_dump_hour     = 1,
+  $graphite_db_dump_monthday = '*',
+  $graphite_db_dump_month    = '*',
+  $graphite_db_dump_weekday  = '*',
+) {
+
+  # We will put database dumps here
+  file { "${graphite_db_dumpdir}":
+    ensure => directory,
+    mode   => '0711',
+    owner  => 'root'
+    group  => 'root',
+  } -> 
+
+  # template using $graphite_db_dumpdir
+  file { '/usr/local/bin/database-dump':
+    ensure  => file,
+    mode    => '0555',
+    owner   => 'root'
+    group   => 'root'
+    content => template("${module_name}/database-dump.erb"),
+  } ->
+
+  cron { 'database-dump':
+    command     => '/usr/local/bin/database-dump',
+    environment => 'PATH=/bin:/usr/bin:/usr/sbin:/usr/local/bin',
+    user        => 'root',
+    minute      => $graphite_db_dump_minute,
+    hour        => $graphite_db_dump_hour,
+    monthday    => $graphite_db_dump_monthday,
+    month       => $graphite_db_dump_month,
+    weekday     => $graphite_db_dump_weekday,
+  }
+}
---
+++ manifests/profile/server/whisper_expire.pp
@@ -0,0 +1,35 @@
+class sys11graphite::profile::server::whisper_expire (
+  # Root of the directory containing your wsp files
+  $whisper_datadir         = '/opt/graphite/storage/whisper';
+  # delete whisper files not written to for 7 days
+  $whisper_expire_days_delay = 7,
+  # execute deletion at this cron timespec
+  $whisper_expire_minute   = 5,
+  $whisper_expire_hour     = 0,
+  $whisper_expire_monthday = '*',
+  $whisper_expire_month    = '*',
+  $whisper_expire_weekday  = '*',
+) {
+
+  # template using $whisper_expire_days_delay and $whisper_datadir in generated cronjob
+  file { '/usr/local/bin/graphite-whisper-expire':
+    ensure  => file,
+    mode    => '0555',
+    owner   => 'root',
+    group   => 'root',
+    content => template("${module_name}/graphite-whisper-expire.erb"),
+  } ->
+
+  # Run that job
+  cron { 'graphite-whisper-expire':
+    command     => '/usr/local/bin/graphite-whisper-expire',
+    environment => 'PATH=/bin:/usr/bin:/usr/sbin:/usr/local/bin',
+    user        => 'www-data',
+    minute      => $whisper_expire_minute,
+    hour        => $whisper_expire_hour,
+    monthday    => $whisper_expire_monthday,
+    month       => $whisper_expire_month,
+    weekday     => $whisper_expire_weekday,
+  }
+
+}
